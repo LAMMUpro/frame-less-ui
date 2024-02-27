49ce58c1c547ad7cb925d49dd21988d726395b4a
陈永超 <chen_yongchao@126.com>
2024-02-27 14:21:53 +0800

    wc-dialog

--- src/components/wc-dialog/index.stories.tsx
+++ src/components/wc-dialog/index.stories.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { define as defineWcDialog } from './index';
 import type { Meta, StoryObj } from '@storybook/web-components';
 
@@ -24,19 +25,50 @@ export default meta;
 
 // 实例1
 export const Primary = {
-  args: {
-    type: 'success',
-    size: 'medium',
-    plain: false,
-    round: false,
-    loading: false,
-    disabled: false,
-    nativeType: 'dialog',
-    text: '按钮实例1'
-  },
+  render: () => {
+    const [show, setShow] = useState(false);
+    return (
+      <div>
+        <wc-button text="显示弹窗" onClick={() => setShow(true)}></wc-button>
+        <wc-dialog 
+          visible={show}
+          closeOnClickModal={true}
+          showClose={true}
+          lockScroll={true}
+          zIndex={1000}
+          title="弹窗标题"
+          width="1000px"
+        >
+          <div >
+            <h2>弹窗内容</h2>
+          </div>
+        </wc-dialog>
+      </div>
+    )
+  }
 };
 
 // 实例2
-export const Secondary: StoryObj = {
-  render: () => (<wc-dialog text="按钮实例2" plain={false} round={true}></wc-dialog>),
-};
+// export const Secondary: StoryObj = {
+//   render: () => {
+//     const [show, setShow] = useState(false);
+//     return (
+//       <div>
+//         <wc-button text="显示弹窗" onClick={() => setShow(true)}></wc-button>
+//         <wc-dialog 
+//           visible={show}
+//           closeOnClickModal={true}
+//           showClose={true}
+//           lockScroll={true}
+//           zIndex={1000}
+//           title="弹窗标题"
+//           width="1000px"
+//         >
+//           <div >
+//             <h2>弹窗内容</h2>
+//           </div>
+//         </wc-dialog>
+//       </div>
+//     )
+//   }
+// };
