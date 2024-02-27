f739be11a6311fd6179e495eea828c45f2a00153
陈永超 <chen_yongchao@126.com>
2024-02-27 19:39:53 +0800

    webcomponent装饰器

---
+++ src/decorator/webcomponent.ts
@@ -0,0 +1,24 @@
+import register from "@/utils/preact-custom-element";
+
+/**
+ * webcomponent类装饰器
+ * @param tagName 自定义标签名称
+ */
+export function WebComponentDefine(tagName: string, options?: {
+  /** 需要监控的组件属性 */
+  watchProps: string[];
+  /** 是否使用shadow dom封装组件 */
+  useShadow: boolean;
+}) {
+  // TODEO，第一个TS参数怎么写 ，这里不知道为什么需要第二个参数
+  return function (WcComponentClass: Function, _:any) {
+    // TODO tagName正则校验: 自定义组件名称，必须含有下划线 
+    /** 定义过了则退出 */
+    if (customElements.get(tagName))
+      return console.warn(`custom element <${tagName}> is used!`);
+    register(WcComponentClass, tagName, options?.watchProps || [], {
+      shadow: options?.useShadow || true,
+    });
+  }
+}
+
