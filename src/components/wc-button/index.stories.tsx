2dc4ed1a7f95b7578013d8221257b8a241a46eed
陈永超 <chen_yongchao@126.com>
2024-02-27 13:12:25 +0800

    wc-button

--- src/components/wc-button/index.stories.tsx
+++ src/components/wc-button/index.stories.tsx
@@ -25,13 +25,18 @@ export default meta;
 // 实例1
 export const Primary = {
   args: {
-    primary: true,
-    label: 'Button',
-    vvv: 12,
+    type: 'success',
+    size: 'medium',
+    plain: false,
+    round: false,
+    loading: false,
+    disabled: false,
+    nativeType: 'button',
+    text: '按钮实例1'
   },
 };
 
 // 实例2
 export const Secondary: StoryObj = {
-  render: () => (<wc-counter vvv={222}></wc-counter>),
+  render: () => (<wc-button text="按钮实例2" plain={false} round={true}></wc-button>),
 };
--- src/components/wc-button/index.tsx
+++ src/components/wc-button/index.tsx
@@ -1,9 +1,32 @@
 import register from "@/utils/preact-custom-element";
 import { Component } from "preact";
 import styleInline from './index.scss?inline';
+import { extractClass } from "@/utils";
+
+type WuButtonType = 'primary' | 'success' | 'warning' | 'danger' | 'info';
+type NativeType = 'button' | 'submit' | 'reset';
+type UISize = 'medium' | 'small' | 'mini';
 
 export interface PropsType {
-  text: number
+  type: WuButtonType;
+
+  size: UISize;
+
+  plain: boolean;
+
+  round: boolean;
+
+  circle: boolean;
+
+  loading: boolean;
+
+  disabled: boolean;
+
+  icon: string;
+
+  nativeType: NativeType;
+
+  text: string;
 }
 
 export interface StateType {
@@ -29,9 +52,27 @@ export class WcButton extends Component<PropsType, StateType> {
 
   render(props: PropsType, state: StateType, context: any) {
     return (
-      <>
-        <button> 按钮 {props.text}</button>
-      </>
+      <button
+          disabled={props.disabled}
+          {...extractClass({}, 'wu-button', {
+              ['wu-button-' + props.type]: props.type,
+              ['wu-button-' + props.size]: props.size,
+              'is-plain': props.plain,
+              'is-round': props.round,
+              'is-circle': props.circle,
+              'is-disabled': props.disabled,
+          })}
+          type={props.nativeType}
+      >
+          {props.loading && [
+              <svg class="loading" viewBox="0 0 1024 1024" focusable="false" data-icon="loading" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+                  <path d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z" />
+              </svg>,
+              ' ',
+          ]}
+          {props.text}
+          <slot />
+      </button>
     );
   }
 }
