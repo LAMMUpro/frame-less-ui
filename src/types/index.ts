be2b80b430a204929f77c2893e8a9f9dd5aa38a1
LAMMU <chen_yongchao@126.com>
2024-03-03 12:56:51 +0800

    ts类型优化

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,7 @@
-import { PropsType as CounterPropsType } from '../components/counter';
+import { Counter, PropsType as CounterPropsType } from '../components/counter';
 import { PropsType as ButtonPropsType } from '../components/button';
 import { PropsType as BadgePropsType } from '../components/badge';
 import { PropsType as DialogPropsType } from '../components/dialog';
-// import { PropsType as TreePropsType } from '../components/tree';
 
 import { Tree } from '../components/tree';
 
@@ -16,7 +15,18 @@ declare module "preact" {
       "fl-button": ButtonPropsType;
       "fl-badge": BadgePropsType;
       "fl-dialog": DialogPropsType;
+      // TODO props类型提取
       // "fl-tree": InstanceType<typeof Tree>
     }
+  }
+}
+
+/**
+ * 定义后document.createElement能获取到其中类型
+ * lit组件才能这么用?
+ */
+declare global {
+  interface HTMLElementTagNameMap {
+    "fl-tree": Tree;
   }
 }
