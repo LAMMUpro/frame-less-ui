7a2fa97bf4d5dae9d9da32fa28fd4bc82062e333
LAMMU <chen_yongchao@126.com>
2024-03-03 14:01:15 +0800

    组件调试3合1

--- src/components/tree/index.test.tsx
+++ src/components/tree/index.test.tsx
@@ -1,16 +1,21 @@
 import { render } from 'preact';
 import { useState } from 'react';
-import('@/components/tree');
+import { createApp } from 'vue';
+import Vue3App from './index.test.vue';
 
-export function App() {
-  const [name, setName] = useState("worl");
-  const arr = ['str',4,5];
+function PreactApp() {
+  const [name, setName] = useState("word preact");
+  const arr = ['str', 4, 5, () => console.log(666)];
 	return (
 		<div>
       {/* 传递Array和object需要先json.stringify!!! */}
-      <fl-tree name={name} arr={JSON.stringify(arr)}></fl-tree>
+      <fl-tree name={name} arr={arr}></fl-tree>
 		</div>
 	);
 }
 
-render(<App />, document.getElementById('app'));
+render(<PreactApp />, document.getElementById('preact'));
+
+
+const vue3App = createApp(Vue3App);
+vue3App.mount('#vue3');
---
+++ src/components/tree/index.test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <fl-tree name="word vue3" :arr="arr"></fl-tree>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const arr = ref([
+  1,{
+    f: () => {console.log(666)}
+  },
+  '2323', 
+  [5],
+  {}
+]);
+</script>
+
+<style lang="scss">
+.main {
+  font-size: 26px;
+}
+</style>
