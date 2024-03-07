8530fc6112f1f6a25a2dfcf300fc5cf85231725c
陈永超 <chen_yongchao@126.com>
2024-03-07 20:12:37 +0800

    测试eventType

--- src/components/tree/index.ts
+++ src/components/tree/index.ts
@@ -4,13 +4,44 @@ import stylesInline from './index.scss?inline';
 import { ct } from '@/utils';
 import { LitWebcomponent } from '@/decorator/webcomponent';
 
+export type EmitType = {
+  /**
+   * @event success
+   * @describe 成功时回调
+   * @detail { list: Array<string> }
+   */
+  (e: 'success', data: { list: Array<string> }): void
+  /**
+   * @event submit
+   * @describe 
+   * 成功时回调
+   * 如果失败则，
+   * 如果成功则......
+   * @detail 
+   * {
+   *  list: Array<string>
+   *  data: Array<string>
+   * }
+   */
+  (e: 'submit'): void
+}
+
 @LitWebcomponent('tree', import('./index.scss?inline'))
 export class Tree extends LitElement {
+  
+  emit: EmitType = (name: string, ...args: any[]) => {
+    this.dispatchEvent(new CustomEvent(name, ...args));
+  }
+
   @property({ reflect: true }) 
   name: string = 'world! ';
 
   @property({ type: Array })
   arr: Array<number> = [];
+
+  submit () {
+    this.emit('success', true);
+  }
 
   render() {
     return html`
