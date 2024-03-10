ff5e3433d801f7f0717e4e2e97c5490af36f6ac4
LAMMU <chen_yongchao@126.com>
2024-03-10 11:45:27 +0800

    复制文本函数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -63,3 +63,36 @@ export function ct(...args: Array<string | {
 export function toCamelCase(str: string) {
 	return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''));
 }
+
+
+/**
+ * 点击复制
+ * @param text 复制的内容
+ * @param success 成功回调
+ * @param fail 出错回调
+ */
+export function copyText(text: string, success?: () => void, fail?: (res: string) => void) {
+  text = text.replace(/(^\s*)|(\s*$)/g, '');
+  if (!text) {
+    fail && fail('复制的内容不能为空！');
+    return;
+  }
+  const id = 'the-clipboard';
+  let clipboard = (document.getElementById(id) as HTMLTextAreaElement);
+  if (!clipboard) {
+    clipboard = document.createElement('textarea');
+    clipboard.id = id;
+    clipboard.readOnly = true;
+    clipboard.style.cssText = 'font-size: 15px; position: fixed; top: -1000%; left: -1000%;';
+    document.body.appendChild(clipboard);
+  }
+  clipboard.value = text;
+  clipboard.select();
+  clipboard.setSelectionRange(0, clipboard.value.length);
+  const state = document.execCommand('copy');
+  if (state) {
+    success && success();
+  } else {
+    fail && fail('复制失败');
+  }
+}
