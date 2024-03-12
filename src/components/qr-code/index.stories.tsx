9e17be09f21f40bc73770ba6f721c521a1d1d8b0
陈永超 <chen_yongchao@126.com>
2024-03-12 13:36:51 +0800

    qr-code组件文档优化

--- src/components/qr-code/index.stories.tsx
+++ src/components/qr-code/index.stories.tsx
@@ -1,55 +1,34 @@
-import { Meta } from '@storybook/web-components';
-import './index';
-import { getDefaultArgs } from '@/utils/storybook';
+import { getDefaultArgs, getArgTypesFromAutoMeta } from '@/utils/storybook';
 import { GlobalConfig } from '@/config';
-import { QrCode } from './index';
-import { useState } from 'react';
 import metaCache from './meta.cache';
+import { StoryMeta } from '@/types/storybook';
+import './index'; // 注册组件
+import { useState } from 'react';
 
-const argTypes:Meta<QrCode>['argTypes'] = metaCache.tableInfo.props.reduce((result, item) => {
-  result[item.name] = {
-    // description: item.describe,
-    control: item.control || 'text',
-    options: JSON.parse(item.options || '[]'),
-    type: {
-      required: item.required == undefined ? false : true,
-    },
-    defaultValue: {
-      summary: item.control === 'number' ? +item.default: item.default,
-    }
-  }
-  return result;
-}, {});
-
-const meta: Meta<QrCode> = {
-  component: `${GlobalConfig.componentPrefix}-qr-code`,
-  subtitle: '二维码图片生成',
-  description: '这是一个实验性功能',
-  /** props定义 */
+const argTypes = getArgTypesFromAutoMeta(metaCache);
+const storyMeta: StoryMeta = {
+  component: `<${GlobalConfig.componentPrefix}-${metaCache.componentName}>`,
+  subtitle: metaCache.subtitle,
+  description: metaCache.description,
   argTypes,
-  /** 默认值设置 */
   args: getDefaultArgs(argTypes),
 }
+export default storyMeta;
 
-export default meta;
+
 
 export const 基本用法 = {
   render: (args) => {
-    const [text, setText] = useState(args.value);
+    const [value, setValue] = useState(args.value);
     function change() {
-      setText(new Date().getTime() + '');
+      setValue(new Date().getTime() + '');
     }
     return (
       <div>
-        <button onClick={change}>变换2</button>
-        <fl-qr-code 
-          value={text}
-          size={args.size}
-          label={args.label}
-          color={args.color}
-          backgroundColor={args.backgroundColor}
-          errorCorrectionLevel={args.errorCorrectionLevel}
-        ></fl-qr-code>
+        <div>
+          <fl-qr-code {...args} value={value} />
+        </div>
+        <button onClick={change}>变换生成的文本</button>
       </div>
     )
   },
