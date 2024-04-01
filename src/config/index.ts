bf2786a450a48b018ad61bca031b27987e0ed05e
LAMMU <chen_yongchao@126.com>
2024-04-02 00:26:07 +0800

    全局变量

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,3 +5,12 @@ export const GlobalConfig = {
   /** 组件前缀 <[前缀]-[组件名]> */
   componentPrefix: 'fl',
 }
+
+/** 全局变量, 没有响应式 */
+export const GlobalVar: {
+  /** 全局popover弹出容器 */
+  FlPopoverContainerDom?: HTMLElement
+} = {
+  FlPopoverContainerDom: undefined,
+}
+
