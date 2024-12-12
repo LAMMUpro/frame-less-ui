7ff2c1f930a9c572356e4f250bbdf82db455058a
陈永超 <chen_yongchao@126.com>
2024-12-12 19:14:18 +0800

    fix: 完善new:comp文件递归复制功能

--- scripts/newVueComponent.ts
+++ scripts/newVueComponent.ts
@@ -36,27 +36,60 @@ async function getComponentName() {
   return componentName;
 }
 
+/**
+ * 递归复制文件夹内的文件
+ * @param src 源文件夹路径
+ * @param dest 目标文件夹路径
+ * @example copyFolderSync('./template/vue-component', './src/components/button')
+ */
+function copyFolderSync(src: string, dest: string, cb?: (content: string) => string) {
+  // 如果目标文件夹不存在则创建
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest);
+  }
+
+  // 读取源文件夹中的所有文件/文件夹
+  const files = fs.readdirSync(src);
+
+  files.forEach(file => {
+    const srcPath = path.join(src, file);
+    const destPath = path.join(dest, file);
+    
+    // 判断是文件还是文件夹
+    if (fs.statSync(srcPath).isDirectory()) {
+      // 如果是文件夹就递归复制
+      copyFolderSync(srcPath, destPath, cb);
+    } else {
+      // 如果是文件就读取文件内容，并将cb(内容)的结果写入到目标文件
+      const content = fs.readFileSync(srcPath, 'utf-8')
+      fs.writeFile(destPath, cb ? cb(content) : content, 'utf-8', ()=> {})
+    }
+  });
+}
+
 async function main() {
   const componentName = await getComponentName();
   const className = toCamelCase(componentName[0].toUpperCase() + componentName.slice(1));
 
-  const subtitle = await getInput('请输入组件副标题：') || '';
-  const describe = await getInput('请输入组件描述：') || '';
-  fs.mkdirSync(path.resolve(componentDir, componentName));
-  // TODO：处理文件夹
-  fs.readdirSync('./template/vue-component').forEach(filename => {
-    let codeStr = fs.readFileSync(path.resolve('./template/vue-component', filename), 'utf-8')
-      .replaceAll('fl-temp-name', `fl-${componentName}`)
-      .replaceAll('temp-name', componentName)
-      .replaceAll('TempName', className);
-    // if (filename === 'index.ts') 
-    //   codeStr = codeStr
-    //     .replace('${组件副标题}', subtitle)
-    //     .replace('${组件描述}', describe);
-    fs.writeFile(path.resolve(componentDir, componentName, filename), codeStr, 'utf-8', ()=> {})
-  })
-  console.log('> 组件文件生成完毕, 请执行yarn build:meta');
+  // const subtitle = await getInput('请输入组件副标题：') || '';
+  // const describe = await getInput('请输入组件描述：') || '';
+  if (!fs.existsSync(path.resolve(componentDir, componentName))) {
+    fs.mkdirSync(path.resolve(componentDir, componentName));
+  }
+  
+  /** 
+   * 复制./template/vue-component文件夹下面所有的文件到./src/components/${componentName}
+   * 包括文件夹下面的文件
+   */
+  copyFolderSync(
+    path.resolve('./template/vue-component'), 
+    path.resolve(componentDir, componentName),
+    (content) => {
+      return content.replaceAll('fl-temp-comp', `fl-${componentName}`)
+        .replaceAll('temp-comp', componentName)
+        .replaceAll('TempComp', className);;
+    }
+  );
 }
-
 
 main();
