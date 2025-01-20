6551ae159062ad8da7866bb09b6ac4e919097e81
陈永超 <chen_yongchao@126.com>
2025-01-20 09:42:44 +0800

    chore: 组件模板优化

--- template/vue-component/index.ce.vue
+++ template/vue-component/index.ce.vue
@@ -5,22 +5,33 @@
 </template>
 
 <script lang="ts" setup>
+import { useAttrs } from 'vue';
+import { EmitType, PropsType, defaultProps } from './utils.ts';
+// 如果需要用到el-popover组件，则需要手动引入
+// import { ElPopover } from 'element-plus';
 
-// const props = defineProps({
-  
-// })
+// @ts-ignore
+const props = withDefaults(defineProps<PropsType>(), defaultProps);
 
-// const emit = defineEmits<{
+const emit = defineEmits<EmitType>();
 
-// }>();
+const otherProps = useAttrs();
 
+/** 用于平替onMounted */
+async function _onMounted() {
+  console.log('>>> temp-comp _onMounted')
+}
 
 defineExpose({
-  
+  _onMounted,
 })
 </script>
 
 <style lang="scss">
 @import '@/styles/common.scss';
+
+// 如果需要用到el-popover组件，则需要手动引入这两个样式文件
+// @import '@/styles/element-plus-base.scss';
+// @import 'element-plus/theme-chalk/el-popover.css';
 
 </style>
